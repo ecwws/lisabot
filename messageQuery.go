45510ed1091d28d5399053c181001380c46d3723
Richard Li <evilcat@wisewolfsolutions.com>
2016-05-23 06:38:16 +0000

    move responder code into separate file

--- messageQuery.go
+++ messageQuery.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"os/exec"
-	"regexp"
 	"strings"
 )
 
@@ -11,6 +9,7 @@ type messageBlock struct {
 	From      string `json:"from,omitempty"`
 	Room      string `json:"room,omitempty"`
 	Mentioned bool   `json:"mentioned,omitempty"`
+	Mention   string `json:"mention,omitempty"`
 }
 
 func (m *messageBlock) handleMessage(source string,
@@ -30,40 +29,10 @@ func (m *messageBlock) handleMessage(source string,
 
 	if prefixMatch {
 		trimmed := strings.TrimLeft(m.Message[conf.prefixLen:], " ")
-		for _, pr := range prefixPResponders {
-			if match, err := regexp.MatchString(pr.Match,
-				trimmed); err == nil && match {
-
-				logger.Debug.Println("Matched:", pr.Match)
-
-				output, err := exec.Command(pr.Cmd, pr.Args...).Output()
-
-				if err != nil {
-					logger.Error.Println("Passive responder error:", err)
-				} else {
-					logger.Debug.Println("Passive responder executed:",
-						string(output))
-
-					dispatch <- &dispatcherRequest{
-						Query: &query{
-							Type:   "message",
-							Source: "PR:" + pr.Match,
-							To:     source,
-							Message: &messageBlock{
-								Message: strings.TrimRight(string(output), "\n"),
-								Room:    m.Room,
-							},
-						},
-					}
-				}
-
-				if !pr.FallThrough {
-					break
-				}
-			}
-		}
+		triggerPassiveResponders(prefixPResponders, trimmed, source, m.Room,
+			m.Mention, m.Mentioned, dispatch)
 	} else {
-		// for _, pr := range noPrefixPResponders {
-		// }
+		triggerPassiveResponders(noPrefixPResponders, m.Message, source, m.Room,
+			m.Mention, m.Mentioned, dispatch)
 	}
 }
